29af729e31b035655f7c84c6e3a95271233e5249
ht21a009 <114827288+ht21a009@users.noreply.github.com>
2023-07-22 01:31:17 +0000

    second commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   
-  config.hosts << "f4de7d4c8dc94f57b3a23d5d84922e89.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.hosts.clear
 end
